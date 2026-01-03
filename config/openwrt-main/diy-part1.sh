d44e05fa6e501769cde93b135b06e29292fcb83b
susanforme <rzc307853639@gmail.com>
2026-01-03 13:39:20 +0800

    更新 diy-part1.sh 和 diy-part2.sh，添加第三方 feeds 和软件包，修改默认 LAN IP 设置，配置 Docker 开机自启

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: main
-#========================================================================================================================
+set -e
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# ========== 第三方 feeds ==========
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
 
-# other
-# rm -rf package/utils/{ucode,fbtest}
+# ========== 更新 feeds ==========
+./scripts/feeds update -a
+./scripts/feeds install -a
 
+# ========== Clash / MosDNS / Lucky ==========
+./scripts/feeds install luci-app-openclash
+./scripts/feeds install mosdns luci-app-mosdns
+./scripts/feeds install lucky luci-app-lucky
+
+# ========== 网络 / 多拨 / NAT ==========
+./scripts/feeds install luci-app-mwan3
+./scripts/feeds install luci-app-natmap
+./scripts/feeds install luci-app-syncdial
+
+# ========== 广告 / 证书 / 定时 ==========
+./scripts/feeds install luci-app-adguardhome
+./scripts/feeds install luci-app-acme
+./scripts/feeds install luci-app-taskplan
+./scripts/feeds install luci-app-timedreboot
+
+# ========== 系统 / 磁盘 / 流量 ==========
+./scripts/feeds install luci-app-diskman
+./scripts/feeds install luci-app-wrtbwmon
+./scripts/feeds install luci-app-netspeedtest
+
+# ========== Docker ==========
+./scripts/feeds install luci-app-dockerman
+
+# ========== 虚拟化 ==========
+./scripts/feeds install open-vm-tools
+
+# ========== 主题 ==========
+./scripts/feeds install luci-theme-argon
--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: main
-#========================================================================================================================
+set -e
 
-# ------------------------------- Main source started -------------------------------
-#
-# Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# ========== 默认 LAN IP ==========
+sed -i 's/192.168.1.1/192.168.5.1/g' \
+package/base-files/files/bin/config_generate
 
-# Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
+# ========== eth0 -> WAN，其余 -> LAN ==========
+sed -i '/config interface '\''lan'\''/,+5d' \
+package/base-files/files/bin/config_generate
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
-#
-# ------------------------------- Main source ends -------------------------------
+sed -i '/config interface '\''wan'\''/,+5d' \
+package/base-files/files/bin/config_generate
 
-# ------------------------------- Other started -------------------------------
-#
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+cat >> package/base-files/files/bin/config_generate << 'EOF'
 
-# coolsnowwolf default software package replaced with Lienol related software package
-# rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
-# svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
+uci set network.lan=interface
+uci set network.lan.proto='static'
+uci set network.lan.ipaddr='192.168.5.1'
+uci set network.lan.netmask='255.255.255.0'
+uci set network.lan.device='br-lan'
 
-# Add third-party software packages (The entire repository)
-# git clone https://github.com/libremesh/lime-packages.git package/lime-packages
-# Add third-party software packages (Specify the package)
-# svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
-# Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
-# sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
+uci add_list network.@device[0].ports='eth1'
+uci add_list network.@device[0].ports='eth2'
+uci add_list network.@device[0].ports='eth3'
 
-# Apply patch
-# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
-#
-# ------------------------------- Other ends -------------------------------
+uci set network.wan=interface
+uci set network.wan.proto='dhcp'
+uci set network.wan.device='eth0'
+EOF
+
+# ========== 默认主题 ==========
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' \
+feeds/luci/collections/luci/Makefile
+
+# ========== Docker 开机自启 ==========
+mkdir -p package/base-files/files/etc/docker
+echo '{ "registry-mirrors": ["https://dockerproxy.com"] }' \
+> package/base-files/files/etc/docker/daemon.json
